8f87f8ae552b08301719c11da4cde57b0f813d25
skinnynoonie <skinnynoonie@users.noreply.github.com>
2024-02-16 15:04:48 -0500

    Untested but cleaned a lot of boilerplate

--- src/main/java/me/skinnynoonie/primitivemapping/util/DottedUtil.java
+++ src/main/java/me/skinnynoonie/primitivemapping/util/DottedUtil.java
@@ -51,11 +51,12 @@ public final class DottedUtil {
      * Sets an element at the path.
      * If a map exists in the middle of the path, the map will be kept.
      * If any keys overlap this operation, then this operation will overwrite those keys.
+     * If the value is {@code null}, then it will be replaced with a {@link PrimitiveNull}.
      *
      * @param map The map to set.
      * @param path The path to use.
      * @param value The value to set at the end of the path.
-     * @throws IllegalArgumentException If any arguments are null.
+     * @throws IllegalArgumentException If the map or path is null.
      */
     public static void set(PrimitiveMap map, String path, PrimitiveElement value) {
         if (map == null) {
@@ -65,7 +66,7 @@ public final class DottedUtil {
             throw new IllegalArgumentException("path can not be null");
         }
         if (value == null) {
-            throw new IllegalArgumentException("value can not be null");
+            value = PrimitiveNull.create();
         }
 
         String[] pathNodes = path.split(Pattern.quote("."));
@@ -90,92 +91,40 @@ public final class DottedUtil {
      * Gets the element and casts the element if possible.
      * If the element at the end of the path is not of the elementClass, then null will be returned.
      */
-    public static <T extends PrimitiveElement> T getAs(PrimitiveMap map, String path, Class<T> elementClass) {
-        return get(map, path).filter(elementClass::isInstance).map(elementClass::cast).orElse(null);
+    public static <T extends PrimitiveElement> Optional<T> getAs(PrimitiveMap map, String path, Class<T> elementClass) {
+        return get(map, path).filter(elementClass::isInstance).map(elementClass::cast);
     }
 
-    /**
-     * Gets the element and casts the element if possible.
-     * If the element at the end of the path is not of the elementClass, then fallback will be returned.
-     *
-     * @throws NullPointerException If fallback is null. Use {@link #getAs(PrimitiveMap, String, Class)} instead.
-     */
-    public static <T extends PrimitiveElement> T getOr(PrimitiveMap map, String path, T fallback) {
-        @SuppressWarnings("unchecked")
-        Class<T> elementClass = (Class<T>) fallback.getClass();
-        return get(map, path).filter(elementClass::isInstance).map(elementClass::cast).orElse(fallback);
+    public static Optional<String> getString(PrimitiveMap map, String path) {
+        return getAs(map, path, PrimitiveString.class).map(PrimitiveString::asString);
     }
 
-    /**
-     * Sets the value at the end of the path to the element specified (the value parameter).
-     * If the element specified is {@code null}, then it will be replaced with {@link PrimitiveNull}.
-     */
-    public static void setString(PrimitiveMap map, String path, String value) {
-        set(map, path, value == null ? PrimitiveNull.create() : PrimitiveString.of(value));
+    public static Optional<Boolean> getBoolean(PrimitiveMap map, String path) {
+        return getAs(map, path, PrimitiveBoolean.class).map(PrimitiveElement::asBoolean);
     }
 
-    public static String getString(PrimitiveMap map, String path) {
-        return getStringOr(map, path, null);
+    public static Optional<Byte> getByte(PrimitiveMap map, String path) {
+        return getAs(map, path, PrimitiveNumber.class).flatMap(element -> emptyIfFail(element::asByte));
     }
 
-    public static String getStringOr(PrimitiveMap map, String path, String fallback) {
-        return get(map, path).filter(PrimitiveElement::isString).map(PrimitiveElement::asString).orElse(fallback);
+    public static Optional<Short> getShort(PrimitiveMap map, String path) {
+        return getAs(map, path, PrimitiveNumber.class).flatMap(element -> emptyIfFail(element::asShort));
     }
 
-    public static boolean getBoolean(PrimitiveMap map, String path) {
-        return getBooleanOr(map, path, false);
+    public static Optional<Integer> getInt(PrimitiveMap map, String path) {
+        return getAs(map, path, PrimitiveNumber.class).flatMap(element -> emptyIfFail(element::asInt));
     }
 
-    public static boolean getBooleanOr(PrimitiveMap map, String path, boolean fallback) {
-        return get(map, path).filter(PrimitiveElement::isBoolean).map(PrimitiveElement::asBoolean).orElse(fallback);
+    public static Optional<Long> getLong(PrimitiveMap map, String path) {
+        return getAs(map, path, PrimitiveNumber.class).flatMap(element -> emptyIfFail(element::asLong));
     }
 
-    public static byte getByte(PrimitiveMap map, String path) {
-        return getByteOr(map, path, (byte) 0);
+    public static Optional<Float> getFloat(PrimitiveMap map, String path) {
+        return getAs(map, path, PrimitiveNumber.class).flatMap(element -> emptyIfFail(element::asFloat));
     }
 
-    public static byte getByteOr(PrimitiveMap map, String path, byte fallback) {
-        return get(map, path).filter(PrimitiveElement::isNumber).flatMap(element -> emptyIfFail(element::asByte)).orElse(fallback);
-    }
-
-    public static short getShort(PrimitiveMap map, String path) {
-        return getShortOr(map, path, (short) 0);
-    }
-
-    public static short getShortOr(PrimitiveMap map, String path, short fallback) {
-        return get(map, path).filter(PrimitiveElement::isNumber).flatMap(element -> emptyIfFail(element::asShort)).orElse(fallback);
-    }
-
-    public static int getInt(PrimitiveMap map, String path) {
-        return getIntOr(map, path, 0);
-    }
-
-    public static int getIntOr(PrimitiveMap map, String path, int fallback) {
-        return get(map, path).filter(PrimitiveElement::isNumber).flatMap(element -> emptyIfFail(element::asInt)).orElse(fallback);
-    }
-
-    public static long getLong(PrimitiveMap map, String path) {
-        return getLongOr(map, path, 0L);
-    }
-
-    public static long getLongOr(PrimitiveMap map, String path, long fallback) {
-        return get(map, path).filter(PrimitiveElement::isNumber).flatMap(element -> emptyIfFail(element::asLong)).orElse(fallback);
-    }
-
-    public static float getFloat(PrimitiveMap map, String path) {
-        return getFloatOr(map, path, 0.0f);
-    }
-
-    public static float getFloatOr(PrimitiveMap map, String path, float fallback) {
-        return get(map, path).filter(PrimitiveElement::isNumber).flatMap(element -> emptyIfFail(element::asFloat)).orElse(fallback);
-    }
-
-    public static double getDouble(PrimitiveMap map, String path) {
-        return getDoubleOr(map, path, 0.0);
-    }
-
-    public static double getDoubleOr(PrimitiveMap map, String path, double fallback) {
-        return get(map, path).filter(PrimitiveElement::isNumber).flatMap(element -> emptyIfFail(element::asDouble)).orElse(fallback);
+    public static Optional<Double> getDouble(PrimitiveMap map, String path) {
+        return getAs(map, path, PrimitiveNumber.class).flatMap(element -> emptyIfFail(element::asDouble));
     }
 
     private static <T> Optional<T> emptyIfFail(Supplier<T> runnable) {
